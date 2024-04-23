340057a8ffbbd26f64ad010ec0a37c7d5f5a44ef
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-04-23 11:52:06 +0900

    #19.7 Reviews (15:41)

--- src/api.ts
+++ src/api.ts
@@ -11,3 +11,7 @@ export const getRoomDetail = ({queryKey}: QueryFunctionContext) => {
   const [_, roomPk] = queryKey;
   return instance.get(`rooms/${roomPk}`).then((response) => response.data);
 };
+export const getRoomReviews = ({queryKey}: QueryFunctionContext) => {
+  const [_, roomPk] = queryKey;
+  return instance.get(`rooms/${roomPk}/reviews`).then((response) => response.data);
+};
--- src/routes/RoomDetail.tsx
+++ src/routes/RoomDetail.tsx
@@ -1,11 +1,17 @@
 import {useQuery} from '@tanstack/react-query';
 import {useParams} from 'react-router-dom';
-import {getRoomDetail} from '../api';
-import {IRoomDetail} from '../types';
-import {Box, Grid, Heading, Skeleton, Image, GridItem} from '@chakra-ui/react';
+import {getRoomDetail, getRoomReviews} from '../api';
+import {IRoomDetail, IReview} from '../types';
+import {Box, Grid, Heading, Skeleton, Image, GridItem, VStack, Text, HStack, Avatar} from '@chakra-ui/react';
+import {FaStar} from 'react-icons/fa';
+
 export default function RoomDetail() {
   const {roomPK} = useParams();
   const {isLoading, data: roomDetailData} = useQuery<IRoomDetail>({
+    queryKey: ['roomPk', roomPK],
+    queryFn: getRoomDetail,
+  });
+  const {isLoading: isReviewsLoading, data: roomReviewsData} = useQuery<IReview[]>({
     queryKey: ['roomPk', roomPK],
     queryFn: getRoomDetail,
   });
@@ -31,6 +37,37 @@ export default function RoomDetail() {
           </GridItem>
         ))}
       </Grid>
+      <HStack width={'40%'} justifyContent={'space-between'} mt={10}>
+        <VStack alignItems={'flex-start'}>
+          <Skeleton isLoaded={!isLoading} height={'30px'}>
+            <Heading fontSize={'2xl'}>House hosted by {roomDetailData?.owner.name}</Heading>
+          </Skeleton>
+          <Skeleton isLoaded={!isLoading} height={'30px'}>
+            <HStack justifyContent={'flex-start'} w="100%">
+              <Text>
+                {roomDetailData?.toilets} toliet{roomDetailData?.toilets === 1 ? '' : 's'}
+              </Text>
+              <Text>∙</Text>
+              <Text>
+                {roomDetailData?.rooms} room{roomDetailData?.rooms === 1 ? '' : 's'}
+              </Text>
+            </HStack>
+          </Skeleton>
+        </VStack>
+        <Avatar name={roomDetailData?.owner.name} size={'xl'} src={roomDetailData?.owner.avatar} />
+      </HStack>
+      <Box mt={10}>
+        <Heading fontSize={'2xl'}>
+          <HStack>
+            <FaStar />
+            <Text>{roomDetailData?.rating}</Text>
+            <Text>∙</Text>
+            <Text>
+              {roomReviewsData?.length} review{roomReviewsData?.length === 1 ? '' : 's'}
+            </Text>
+          </HStack>
+        </Heading>
+      </Box>
     </Box>
   );
 }
